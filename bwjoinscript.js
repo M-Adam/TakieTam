90a13d41593d9c646e26a70533eeb0cf92794a78
Adam Mołczan <M-Adam@users.noreply.github.com>
2018-05-31 22:04:00 +0200

    Update bwjoinscript.js

--- bwjoinscript.js
+++ bwjoinscript.js
@@ -1,15 +1,13 @@
-// Here You can type your custom JavaScript...
+const poziomPow99 = false;
 var toParse = $('div.expbar').attr('onmouseover');
-var level =parseInt(toParse.substr(toParse.indexOf('POZIOM ') + 'POZIOM '.length, 2));
+var level =parseInt(toParse.substr(toParse.indexOf('POZIOM ') + 'POZIOM '.length, poziomPow99 ? 3 : 2));
 
-var checkboxes = $('span.expedition-invite-list-entry').each(function(i,j){
-    var checkbox = $(j).children()[0];
+$('span.expedition-invite-list-entry').each(function(i,j){
+    var checkbox = $($(j).children()[0]);
     var levelToInvite = parseInt($($($(j).children()[1]).children()[0]).children()[0].textContent);
     if(levelToInvite <= level){
-        $(checkbox).prop('checked', true);
-    }
-    else{
-        $(checkbox).prop('checked', false);
-    }
-    
+        checkbox.prop('checked', true);
+    }else{
+        checkbox.prop('checked', false);
+    } 
 });
